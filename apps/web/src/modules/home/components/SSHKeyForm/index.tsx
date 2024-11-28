188fa07895227f0909e6e4735b120aec23cba839
Jay <jaychong1203@gmail.com>
2024-11-28 08:48:40 -0800

    placeholder, error messages, and download modal popup

--- apps/web/src/modules/home/components/SSHKeyForm/index.tsx
+++ apps/web/src/modules/home/components/SSHKeyForm/index.tsx
@@ -1,123 +1,113 @@
-/* eslint-disable no-use-before-define */
 import { useEffect, useState } from 'react';
-
 import { http } from '@utils/axios';
-
 import './index.css';
 
 type SSHKeyFormProps = {
-	onSubmit: any;
-	externalError: any;
+    onSubmit: any;
+    externalError: any;
 };
 
 const SSHKeyForm = ({ onSubmit, externalError }: SSHKeyFormProps) => {
-	const [sshPrivKey, setSSHPrivKey] = useState('');
-	const [sshPubKey, setSSHPubKey] = useState('');
-	const [error, setError] = useState('');
-	const [status, setStatus] = useState('');
+    const [sshPrivKey, setSSHPrivKey] = useState('');
+    const [sshPubKey, setSSHPubKey] = useState('');
+    const [error, setError] = useState('');
+    const [status, setStatus] = useState('');
 
-	useEffect(() => {
-		if (externalError) {
-			setError(externalError);
-			clearError();
-		}
-	}, [externalError]);
+    useEffect(() => {
+        if (externalError) {
+            setError(externalError);
+            clearError();
+        }
+    }, [externalError]);
 
-	const handleSubmit = async (e: any) => {
-		e.preventDefault();
-		setError('');
-		setStatus('Checking SSH key formatting...');
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        setError('');
+        setStatus('Checking SSH key formatting...');
 
-		setTimeout(async () => {
-			if (!sshPrivKey.trim() || !sshPubKey.trim()) {
-				setError('Please fill out all fields.');
-				setStatus('');
-				clearError();
-				clearStatus();
-				return;
-			}
+        setTimeout(async () => {
+            if (!sshPrivKey.trim() || !sshPubKey.trim()) {
+                setError('Please fill out all fields.');
+                setStatus('');
+                return;
+            }
 
-			try {
-				setStatus('Comparing fingerprints...');
+            try {
+                setStatus('Comparing fingerprints...');
+                const response = await http.post(`/keys/validate`, {
+                    privKey: sshPrivKey,
+                    pubKey: sshPubKey,
+                });
 
-				console.log('Check here');
+                if (response.data?.data?.valid) {
+                    onSubmit({
+                        sshPrivKey,
+                        sshPubKey,
+                        keyType: 'Some key type',
+                        fingerprintValidated: response.data?.data?.fingerprintValidated,
+                    });
+                    setSSHPrivKey('');
+                    setSSHPubKey('');
+                    clearStatus();
+                    clearError();
+                } else {
+                    setError(response.data?.message);
+                    setStatus('');
+                    clearStatus();
+                }
+            } catch (error: any) {
+                console.error('Error handling form submission:', error);
+                if (error.response) {
+                    console.error('Server response:', error.response.data);
+                }
+                setError('Failed to submit SSH keys. Please try again.');
+                setStatus('');
+                clearStatus();
+            }
+        }, 1000);
+    };
 
-				const response = await http.post(`/keys/validate`, {
-					privKey: sshPrivKey,
-					pubKey: sshPubKey,
-				});
+    const clearError = () => {
+        setTimeout(() => setError(''), 2000);  
+    };
 
-				console.log('Fingerprint Calculation Response:', response.data);
+    const clearStatus = () => {
+        setTimeout(() => setStatus(''), 2000);  
+    };
 
-				if (response.data?.data?.valid) {
-					onSubmit({
-						sshPrivKey,
-						sshPubKey,
-						keyType: 'Some key type',
-						fingerprintValidated: response.data?.data?.fingerprintValidated,
-					});
-					setSSHPrivKey('');
-					setSSHPubKey('');
-					clearStatus();
-					clearError();
-				} else {
-					setError('The private and public keys do not match.');
-					setStatus('');
-					clearError();
-					clearStatus();
-				}
-			} catch (error: any) {
-				console.error('Error handling form submission:', error);
-				if (error.response) {
-					console.error('Server response:', error.response.data);
-				}
-				setError('Failed to submit SSH keys. Please try again.');
-				setStatus('');
-				clearError();
-				clearStatus();
-			}
-		}, 1000);
-	};
-
-	const clearError = () => {
-		setTimeout(() => setError(''), 2000);
-	};
-
-	const clearStatus = () => {
-		setTimeout(() => setStatus(''), 2000);
-	};
-
-	return (
-		<div className="ssh-key-form">
-			<form onSubmit={handleSubmit}>
-				<div className="form-group">
-					<label>Private Key:</label>
-					<textarea
-						value={sshPrivKey}
-						onChange={(e) => setSSHPrivKey(e.target.value)}
-						required
-						rows={4}
-						cols={50}
-					/>
-				</div>
-				<div className="form-group">
-					<label>Public Key:</label>
-					<textarea
-						value={sshPubKey}
-						onChange={(e) => setSSHPubKey(e.target.value)}
-						required
-						rows={4}
-						cols={50}
-					/>
-				</div>
-				<button type="submit" className="submitButton">
-					Submit
-				</button>
-			</form>
-			{status && !error && <p className="status">{status}</p>}
-			{error && <p className="error">{error}</p>}
-		</div>
-	);
+    return (
+        <div className="ssh-key-form">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Private Key:</label>
+                    <textarea
+                        value={sshPrivKey}
+                        onChange={(e) => setSSHPrivKey(e.target.value)}
+                        required
+                        rows={10}
+                        cols={60}
+                        placeholder="-----BEGIN TYPE"
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Public Key:</label>
+                    <textarea
+                        value={sshPubKey}
+                        onChange={(e) => setSSHPubKey(e.target.value)}
+                        required
+                        rows={4}
+                        cols={60}
+                        placeholder="ssh-type"
+                    />
+                </div>
+                <button type="submit" className="submitButton">
+                    Submit
+                </button>
+            </form>
+            {status && !error && <p className="status">{status}</p>}
+            {error && <p className="error">{error}</p>}
+        </div>
+    );
 };
 
 export default SSHKeyForm;
--- apps/web/src/modules/home/pages/home/index.tsx
+++ apps/web/src/modules/home/pages/home/index.tsx
@@ -5,14 +5,15 @@ import { http } from '@utils/axios';
 
 import InfoModal from '../../components/InfoModal';
 import SSHKeyForm from '../../components/SSHKeyForm';
+import DownloadInstructionsModal from '../../components/Instruction';
 
-// dotStream.register();
 
 export const Page = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState('');
 	const [success, setSuccess] = useState('');
 	const [showModal, setShowModal] = useState(true);
+	const [showDownloadModal, setShowDownloadModal] = useState(false); 
 
 	const handleCloseModal = () => {
 		setShowModal(false);
@@ -47,7 +48,7 @@ export const Page = () => {
 			}
 
 			console.log('Submitted SSH key successfully:', response.data);
-			setSuccess('Submission successful');
+			setSuccess('Your keys have been submitted');
 			clearSuccess();
 		} catch (error: any) {
 			console.error('Error:', error);
@@ -60,57 +61,35 @@ export const Page = () => {
 
 	const handleDownloadJSON = async () => {
 		try {
-			const response = await fetch(
-				`${process.env.REACT_APP_BACKEND_URL}/api/generatefullData`,
-			);
-			if (!response.ok) {
-				throw new Error('Failed to generate JSON.');
-			}
-			const data = await response.json();
-			const blob = new Blob([JSON.stringify(data, null, 2)], {
-				type: 'application/json',
+			const response = await http.get(`/keys/krl`, {
+				responseType: 'blob',
 			});
-			const url = window.URL.createObjectURL(blob);
-			const a = document.createElement('a');
-			a.style.display = 'none';
-			a.href = url;
-			a.download = 'revocation-list.json';
-			document.body.appendChild(a);
-			a.click();
-			window.URL.revokeObjectURL(url);
-		} catch (error) {
-			console.error('Error downloading JSON file:', error);
-			setError('Failed to download JSON file.');
-			clearError();
-		}
-	};
-
-	const handleDownloadPubKeys = async () => {
-		try {
-			const response = await fetch(
-				`${process.env.REACT_APP_BACKEND_URL}/api/generatepubKey`,
-			);
-			if (!response.ok) {
-				throw new Error('Failed to generate public keys.');
+	
+			if (response.status === 400) {
+				setError('Error when generating the KRL file');
+				clearError();
+				return;
 			}
-			const blob = await response.blob();
-			const url = window.URL.createObjectURL(blob);
-			const a = document.createElement('a');
-			a.style.display = 'none';
-			a.href = url;
-			a.download = 'public-keys.json';
-			document.body.appendChild(a);
-			a.click();
-			window.URL.revokeObjectURL(url);
+	
+			const file = new Blob([response.data], {
+				type: 'application/x-mspublisher',
+			});
+	
+			const link = document.createElement('a');
+			link.href = URL.createObjectURL(file);
+			link.download = 'krl_combined.krl.pub';
+			link.click();
+	
+			setShowDownloadModal(true);
 		} catch (error) {
-			console.error('Error downloading public keys file:', error);
-			setError('Failed to download public keys file.');
+			console.error('Error when generating the KRL file:', error);
+			setError('Error when generating the KRL file');
 			clearError();
 		}
 	};
 
 	const clearError = () => {
-		setTimeout(() => setError(''), 3000);
+		setTimeout(() => setError(''), 5000);
 	};
 
 	const clearSuccess = () => {
@@ -124,13 +103,9 @@ export const Page = () => {
 			</h1>
 			<div className="app-container">
 				{showModal && <InfoModal onClose={handleCloseModal} />}
+				{showDownloadModal && <DownloadInstructionsModal onClose={() => setShowDownloadModal(false)} />}
 				<SSHKeyForm onSubmit={handleFormSubmit} externalError={error} />
 			</div>
-			{isLoading && (
-				<div className="loading-container">
-					<p className="loading-text">Submitting your key</p>
-				</div>
-			)}
 			{!isLoading && success && (
 				<div className="success-container">
 					<p className="success-message">{success}</p>
@@ -138,14 +113,10 @@ export const Page = () => {
 			)}
 			<div className="button-container">
 				<button onClick={handleDownloadJSON} className="appButton">
-					Download Full Data
-				</button>
-				<button onClick={handleDownloadPubKeys} className="appButton">
-					Download Public Keys
+					Download OpenSSH KRL
 				</button>
 			</div>
 		</div>
 	);
-};
-
+}
 export default Page;
